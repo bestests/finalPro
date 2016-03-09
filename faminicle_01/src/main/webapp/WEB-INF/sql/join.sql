efc80f62570632c78672bd98a8fa0c4e4b89201b
jongin89 <mysky1802@naver.com>
2016-03-09 18:46:06 +0900

    sql

--- faminicle_01/src/main/webapp/WEB-INF/sql/join.sql
+++ faminicle_01/src/main/webapp/WEB-INF/sql/join.sql
@@ -1,15 +1,27 @@
-CREATE TABLE FAMINICLE_MEMBER(
-	ID				VARCHAR2(60)		PRIMARY KEY
-	,PASS			VARCHAR2(60)		NOT NULL
-	,E_MAIL			VARCHAR2(100)		NOT NULL
-	,NAME			VARCHAR2(20)		NOT NULL
-	,BIRTHDAY		VARCHAR2(20)		NOT NULL
-	,PHONE_NUMBER	NUMBER(11)			NOT NULL
+CREATE TABLE FAM_MEMBER(
+	MEM_NO 			INT 	NOT NULL 	PRIMARY KEY		  		AUTO_INCREMENT 
+	,ID	 			VARCHAR(20)	 	NOT NULL 
+	,PASS	 		VARCHAR(20)	 	NOT NULL 
+	,E_MAIL		 	VARCHAR(100) 	NOT NULL  
+	,NAME	 		VARCHAR(20)	 	NOT NULL
+	,BIRTHDAY	 	VARCHAR(20)	 	NOT NULL 
+	,TEL	 		INT		 		NOT NULL 
 )
 
 CREATE SEQUENCE SEQ_MEMBER_NO;
 
+ALTER TABLE FAMINICLE_MEMBER ADD MEM_NO INT auto_increment primary key
+
+insert into fam_member(id,pass,e_Mail,name,birthday,tel)
+		values("test","test","test@test","test","test","1234")
+
+
+
+
 /*
+	show full columns from 테이블명 ; //컬럼명 조회하기
+
+
 	사용자 아이디
 	사용자 비밀번호
 	사용자 이메일
