d2f73405b424a74a4b28c95aef9f3c8897b00a52
leesungkyu11 <gkgkaa@nate.com>
2016-03-18 10:56:49 +0900

    ^^

--- faminicle_01/src/main/webapp/WEB-INF/sql/join.sql
+++ faminicle_01/src/main/webapp/WEB-INF/sql/join.sql
@@ -3,14 +3,17 @@ CREATE TABLE FAM_MEMBER(
 	,ID	 			VARCHAR(20)	 	NOT NULL 
 	,PASS	 		VARCHAR(20)	 	NOT NULL 
 	,E_MAIL		 	VARCHAR(100) 	NOT NULL  
+	,member_pic_path VARCHAR(100)  
 	,NAME	 		VARCHAR(20)	 	NOT NULL
 	,BIRTHDAY	 	VARCHAR(20)	 	NOT NULL 
 	,TEL	 		INT		 		NOT NULL 
+	  
 )
 
 CREATE SEQUENCE SEQ_MEMBER_NO;
 
-ALTER TABLE FAMINICLE_MEMBER ADD MEM_NO INT auto_increment primary key
+ALTER TABLE FAM_MEMBER ADD MEM_NO INT auto_increment primary key
+ALTER TABLE FAM_MEMBER ADD login_pic_path varchar(100);
 
 insert into fam_member(id,pass,e_Mail,name,birthday,tel)
 		values("test","test","test@test","test","test","1234")
